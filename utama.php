8f84656b8b71fcce5ec1a2f03e2db3acf7a88bc0
Silvia Kemala <66870453+Silvia-10@users.noreply.github.com>
2020-06-19 07:24:37 -0700

    Create utama.php

---
+++ utama.php
@@ -0,0 +1,21 @@
+		<br>
+		<h2>
+			<center>
+			<font color="#043d07"> Selamat Datang di CETAK FOTO</font>
+			</center>
+		</h2>
+		<h2>
+			<center>
+			<font color="#043d07"> Jl.IAIN no.1 </font>
+			</center>
+		</h2>
+        <marquee> 
+			<img src="assets/images/polaroid.jpg">
+			<img src="assets/images/pola.jpg">
+			<img src="assets/images/">
+		</marquee>
+
+		<br> <br>
+
+		
+	
