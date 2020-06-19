03ad2154cd2f2a5d1d6480dbeae4136840e29255
Silvia Kemala <66870453+Silvia-10@users.noreply.github.com>
2020-06-19 07:20:28 -0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,6 @@
+<a href="index.php">HOME</a>
+<a href="album.php?halaman=produk">ALBUM</a>
+<a href="photo.php?halaman=kategori">PHOTO</a>
+<a href="post.php?halaman=keranjang">POST</a>
+<a href="category.php?halaman=inputmahasiswa">CATEGORY</a>
+<a class="logout" href="logout.php">LOGOUT</a>
